7dbea542b0a5143117e17848e19ff9ece5194022
Leonidas Spyropoulos <artafinde@gmail.com>
2012-01-24 21:32:43 +0000

    Right-click menu added

--- rc.lua
+++ rc.lua
@@ -102,6 +102,23 @@ for s = 1, screen.count() do
 end
 -- }}}
 
+-- {{{ Menu
+-- Create a laucher widget and a main menu
+myawesomemenu = {
+   { "manual", terminal .. " -e man awesome" },
+   { "edit config", editor_cmd .. " " .. awesome.conffile },
+   { "restart", awesome.restart },
+   { "quit", awesome.quit }
+}
+
+mymainmenu = awful.menu({ items = { { "awesome", myawesomemenu, beautiful.awesome_icon },
+                                    { "open terminal", terminal }
+                                  }
+                        })
+
+mylauncher = awful.widget.launcher({ image = image(beautiful.awesome_icon),
+                                     menu = mymainmenu })
+-- }}}
 
 -- {{{ Wibox
 --
@@ -373,6 +390,7 @@ end
 
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
+	awful.button({ }, 3, function () mymainmenu:toggle() end),
     awful.button({ }, 4, awful.tag.viewnext),
     awful.button({ }, 5, awful.tag.viewprev)
 ))
